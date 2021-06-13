8dc6cf2b0e5c337635b35d685d2e7b65171e2508
DB <deenbandhu@swiflearn.com>
2021-06-13 16:55:12 +0530

    added views

---
+++ messenger_backend/messenger_backend/views.py
@@ -0,0 +1,35 @@
+import json
+
+import jwt
+from django.http import JsonResponse
+from rest_framework.views import APIView
+from django.core import serializers
+
+from messenger_backend.models import app_user
+from messenger_backend.settings import SESSION_SECRET
+
+
+class Login(APIView):
+    def post(self,request):
+        body = request.data
+        if not body.get("username","") or not body.get("password",""):
+            return JsonResponse({
+                'error': "Username, password, and email required",
+            }, status=400)
+        user = app_user.objects.filter(username=body["username"])
+        if not user.exists():
+            return JsonResponse({
+                "error": f'No user found for username: {body["username"]}',
+            }, status=401)
+        user = user.first()
+        if not user.verify_password(body["password"]):
+            return JsonResponse({
+                "error": "Wrong username and/or password",
+            }, status=401)
+        token = jwt.encode({"id": user.id}, SESSION_SECRET, algorithm="HS256")
+        # user.token = token
+        user_json = serializers.serialize('json', [user,])
+        user_dict = json.loads(user_json)
+        user_res = user_dict[0]["fields"]
+        user_res["token"] = token
+        return JsonResponse(user_res, status=200)
